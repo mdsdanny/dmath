c9dfe4f1b6716ccb9f31b4a6ea9c302243746cc0
Daniela Sánchez <dannuu.rs@gmail.com>
2024-12-15 22:49:40 -0300

    Adding functions and docstrings

--- dmath.py
+++ dmath.py
@@ -4,48 +4,100 @@ implemented in cmath c module.
 """
 import cmath, math
 
-class DMath():
+class DMath:
 
-    def __init__(self, z):        
+    roots: list[float]
+    n: int
+    z: complex
+
+    def __init__(self, z):
         """
         Initializes z as complex number in an instance attribute.-
         if z is in polar format then it converts z into binomial format.
-        Also, ro and roots instance variables takes default values.
+        Also, n and roots instance variables takes default values.
+        :param z: the complex number
         """
         if type(z) == tuple:
-            z = cmath.rect(z)
+            z = self.binomial(z[0], z[1])
         self.z = z
-        self.ro = None
+        self.n = 0
         self.roots = []
 
-    def n_roots(self, n, degrees=False):
+    def n_roots(self, n):
         """
         It calculates the n number of roots of a z complex number.
-        It ilustrates the The Moivre theorem.
+        It illustrates the The Moivre theorem.
+        :param n: the number of roots
+        :return: a tuple of roots
         """
         if type(n) != int:
             n = int(n)
         if n == 0:
             raise ValueError('Missing n as number of roots')
         
-        n = abs(n)
-        polar = cmath.polar(self.z)
-        self.ro = polar[0] ** (1/n)
+        self.n = abs(n)
+        polar = self.polar()
         fi = polar[1]
         k = 0
         while k < n:
-            zeta = (fi + 2 * k * cmath.pi) / n
-            if degrees:
-                zeta = math.degrees(zeta)
+            zeta = (fi + 2 * k * self.pi()) / self.n
             self.roots.append(zeta)
             k += 1
 
-        return (self.ro,) + tuple(self.roots)
+        return tuple(self.roots)
+
+    def format_roots(self, degrees=False):
+        """
+        It formats the founded roots
+        :param degrees: By default is False
+        :return: a tuple of angles
+        """
+        if degrees:
+            d = []
+            for r in self.roots:
+                d.append(math.degrees(r))
+            return tuple(d)
+        else:
+            return tuple(self.roots)
+
+    def n(self):
+        """
+        Returns the n number of roots
+        """
+        return self.n
+
+    def polar(self):
+        """
+        if z is in binomial format then it converts z into polar format.
+        :return: a tuple with ro and fi in polar format
+        """
+        if type(self.z) == complex:
+            return cmath.polar(self.z)
+        return self.z
+
+    @staticmethod
+    def binomial(mod, ang):
+        """
+        if module and angle are set then returns the binomial format of a polar complex number
+        :param mod: module of a complex polar
+        :param ang: angle of a complex polar
+        :return: the complex in binomial format
+        """
+        if mod and ang:
+            return cmath.rect(mod, ang)
+        return None
+
+    @staticmethod
+    def pi():
+        """
+        Searches for a pi value
+        :return: the cmath.pi value
+        """
+        return cmath.pi
 
     def __str__(self):
         pass
 
     def __repr__(self):
         pass
-
 
